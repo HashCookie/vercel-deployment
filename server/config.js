97fd0e537073193e18e7b65e96ffdd71fe001489
hashcookie <hashcookie404@gmail.com>
2024-06-07 16:45:08 +0800

    chore(config): update APP_KEY and APP_SECRET

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  APP_KEY: "62c2cdf10e77d810",
-  APP_SECRET: "OIQC0HxQzq0dT7ItonlA7z1DMTYFdGxp",
+  APP_KEY: "054613fac2e84b30",
+  APP_SECRET: "XcWBcEhkHOdHR6A8vs3wcBuARtYVvmrS",
   API_URL: "https://openapi.youdao.com/v2/correct_writing_text",
   PORT: process.env.PORT || 3000,
 };
